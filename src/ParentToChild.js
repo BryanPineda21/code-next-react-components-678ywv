6f8579725b3c9761cceb6f02208c9f7576353976
BryanPineda21 <109675212+BryanPineda21@users.noreply.github.com>
2023-06-02 16:28:36 -0400

    Initial commit

---
+++ src/ParentToChild.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+// TODO: Without changing anything, what does this code do?
+export const ParentToChild = () => {
+  const [msg, setMsg] = useState('');
+
+  // TODO: Update this code so that the fooified and barified versions of
+  // the message are rendered when the input is changed.
+
+  return (
+    <>
+      <h2>ParentToChild</h2>
+
+      <div>
+        <input id="msg" className="form-control" />
+      </div>
+
+      <div>msg: {msg}</div>
+
+      <Fooifier msg={msg} />
+    </>
+  );
+};
+const Fooifier = (props) => {
+  console.log(props);
+  const msg = props.msg;
+  const msgWithFoo = `${msg} foo`;
+
+  return (
+    <>
+      <div>fooified: {msgWithFoo}</div>
+      <Barifier />
+    </>
+  );
+};
+
+const Barifier = (props) => {
+  const msg = props.msg;
+  const msgWithBar = `${msg} bar`;
+
+  return (
+    <>
+      <div>barified: {msgWithBar}</div>
+    </>
+  );
+};
